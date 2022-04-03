cd42df1bccd2abd1d286a2f42a5f271dc5e48f87
paulosergei4 <ppshostka@gmail.com>
2022-04-03 22:23:36 +0300

    Add content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -140,7 +140,7 @@
                         <h3><a href="javascript:void(0);">Соціальна складова</a></h3>
                         <h5>соціальні проекти</h5>
                         <p>
-                            Sunt explicabo. Nemo enim ipsam voluptatem quia sit voluptas aspernatur.
+                            My explicabo. Nemo enim ipsam voluptatem quia sit voluptas aspernatur.
                         </p>
                     </div>
                 </div>
